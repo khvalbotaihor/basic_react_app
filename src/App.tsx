1d8caf0789fba28d201715865ec0d417bd2e4fa4
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-09-08 01:51:47 +0300

    2 lesson - create AccordionTitle and AccordionBody components

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,6 @@ function App() {
                 <AppTitle />
                 <Rating/>
                 <Accordion/>
-
                 <Rating/>
             </div>
         </div>
@@ -57,14 +56,27 @@ function Accordion() {
     console.log("Accordion rendering")
     return (
         <div>
-            <h3>Menu</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
+            <AccordionTitle />
+            <AccordionBody />
         </div>
     )
+}
+
+function AccordionTitle() {
+    console.log("AccordionTitle rendering")
+return(
+    <h3>Menu</h3>
+)
+}
+function AccordionBody() {
+    console.log("AccordionBody rendering")
+return(
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+)
 }
 
 //function expression
