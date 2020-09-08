694c81b79adc0496474e9ede0574ba6e44a7e59a
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-09-08 17:53:55 +0300

    5 lesson - refactor Rating component

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,13 @@ function App() {
                 <Accordion title={"Menu"} collapsed={true}/>
                 <Accordion title={"Users"} collapsed={false}/>
 
+
+                <Rating value={0} />
+                <Rating value={1} />
+                <Rating value={2} />
+                <Rating value={3} />
+                <Rating value={4} />
+                <Rating value={5} />
             </div>
         </div>
     );
