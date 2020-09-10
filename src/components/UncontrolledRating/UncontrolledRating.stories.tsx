aff893458e04224ee489a6ac888f06fac77e10f5
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-09-10 23:57:23 +0300

    10 lesson - add UncontrolledRating storybook

---
+++ src/components/UncontrolledRating/UncontrolledRating.stories.tsx
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import {action} from "@storybook/addon-actions";
+import {UncontrolledRating} from "./UncontrolledRating";
+
+export default {
+    title: 'UncontrolledRating stories',
+    component: UncontrolledRating
+}
+const callback = action("accordion mode change event fired")
+
+export const UncontrolledRatin = () => <UncontrolledRating />
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
