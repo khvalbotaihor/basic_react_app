d6adc5ebcb456da82aa0df554bd5760ac7651c86
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-09-10 23:57:40 +0300

    10 lesson - add UncontrolledAccordion storybook

---
+++ src/components/UncontrolledAccordion/UncontrolledAccordion.stories.tsx
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+import {action} from "@storybook/addon-actions";
+import UncontrolledAccordion from "./UncontrolledAccordion";
+
+
+export default {
+    title: 'UncontrolledAccordion stories',
+    component: UncontrolledAccordion
+}
+const callback = action("accordion mode change event fired")
+
+export const MenuCollapsedMode = () => <UncontrolledAccordion title={"Menu"} />
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
