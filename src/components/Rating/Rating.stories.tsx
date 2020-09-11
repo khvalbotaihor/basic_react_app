35d94465788d19259d512665fa3564a489516d47
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-09-11 11:23:50 +0300

    10 lesson - update Rating stories component

--- src/components/Rating/Rating.stories.tsx
+++ src/components/Rating/Rating.stories.tsx
@@ -1,87 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState} from "react";
 import {Rating, RatingValueType} from "./Rating";
+import {action} from "@storybook/addon-actions";
 
 export default {
-    title: 'Rating stories',
+    title: "Rating component",
     component: Rating
 }
 
-export const EmptyStars = () => <Rating value={0} onClick={x=>x} />
-export const Rating1 = () => <Rating value={1} onClick={x=>x} />
-export const Rating2 = () => <Rating value={2} onClick={x=>x} />
-export const Rating3 = () => <Rating value={3} onClick={x=>x} />
-export const Rating4 = () => <Rating value={4} onClick={x=>x} />
-export const Rating5 = () => <Rating value={5} onClick={x=>x} />
-export const RatingChanging = () => {
-    const [rating, setRating] = useState<RatingValueType>(3);
-    return <Rating value={rating} onClick={setRating} />
+const callback = "Clicked rating component";
+
+export const UncontrolledRating = () => <Rating value={2} onClick={action(callback)} />
+export const SwitchRatingValues = () => {
+    const[star, setStar] = useState<RatingValueType>(2);
+    return <Rating value={star} onClick={setStar} />
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-
-const Template: Story<ButtonProps> = (args) => <Button {...args} />;
-
-export const Primary = Template.bind({});
-Primary.args = {
-    primary: true,
-    label: 'Button',
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-    label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-    size: 'large',
-    label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-    size: 'small',
-    label: 'Button',
-};
-*/
