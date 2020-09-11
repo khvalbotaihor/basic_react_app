6aee5d21031e86009823329bb1f601bdc8bfaff7
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-09-11 11:25:25 +0300

    10 lesson - update UcontrolledOnOff stories component

--- src/components/UncontrolledOnOff/UncontrolledOnOff.stories.tsx
+++ src/components/UncontrolledOnOff/UncontrolledOnOff.stories.tsx
@@ -1,52 +1,14 @@
-import React, {useState} from 'react';
-import {action} from "@storybook/addon-actions";
+import React from "react";
 import UncontrolledOnOff from "./UncontrolledOnOff";
-
+import {action} from "@storybook/addon-actions";
 
 export default {
-    title: 'UncontrolledOnOff stories',
+    title: "Uncontrolled OnOff",
     component: UncontrolledOnOff
 }
-const callback = action("accordion mode change event fired")
 
-export const MenuCollapsedMode = () => <UncontrolledOnOff onChange={x=>x} />
+const callback= "Clisked OnOff component";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export const OnMode = () =>  <UncontrolledOnOff defaultOn={true} onChange={action(callback)} />
+export const OffMode = () =>  <UncontrolledOnOff defaultOn={false} onChange={action(callback)} />
+export const BugMode = () =>  <div>Unsync when change default value when already rendered</div>
