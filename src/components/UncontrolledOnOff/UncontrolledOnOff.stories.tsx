9b6b0f5524ca521d51289b58371c9073b6764bf2
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-09-14 17:59:26 +0300

    10 lesson - add stories for Uncontrolled OnOff component

--- src/components/UncontrolledOnOff/UncontrolledOnOff.stories.tsx
+++ src/components/UncontrolledOnOff/UncontrolledOnOff.stories.tsx
@@ -3,12 +3,11 @@ import UncontrolledOnOff from "./UncontrolledOnOff";
 import {action} from "@storybook/addon-actions";
 
 export default {
-    title: "Uncontrolled OnOff",
+    title: "Uncontrolled OnOff component",
     component: UncontrolledOnOff
 }
 
-const callback = action("Button was clicked")
+const callback = action("Button was clicked");
 
-export const OnSwitched = () => <UncontrolledOnOff defaultOn={true} onChange={callback} />
-export const OffSwitched = () => <UncontrolledOnOff defaultOn={false} onChange={callback} />
-export const BugEffect = () => <div>Unsync when change efault value when already entered</div>
+export const DefaultOn = () => <UncontrolledOnOff defaultValue={true} onChange={callback} />
+export const DefaultOff = () => <UncontrolledOnOff defaultValue={false} onChange={callback} />
