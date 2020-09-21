7015db3dc65053b5104fdd30d2e47b924ccdcbc3
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-09-21 17:59:38 +0300

    15 lesson - add select component

---
+++ src/components/Select/Select.stories.tsx
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+import {Select} from "./Select";
+import {action} from "@storybook/addon-actions";
+
+export default {
+    title: "Select",
+    component: Select
+}
+
+
+export const WithValue = () => {
+    const [value, setValue] = useState("2");
+
+    return <>
+        <Select
+            value={value}
+            onChange={setValue}
+            items={
+                [{value: "1", title: "Minsk"},
+                    {value: "2", title: "Kiev"},
+                    {value: "3", title: "Moskov"},
+                ]
+            }/>
+
+    </>
+}
+
+export const WithoutValue = () => {
+    const [value, setValue] = useState(undefined);
+
+
+    return <>
+        <Select
+            onChange={setValue}
+            value={value}
+            items={
+                [{value: "1", title: "Minsk"},
+                    {value: "2", title: "Kiev"},
+                    {value: "3", title: "Moskov"},
+                ]
+            }/>
+
+    </>
+}
