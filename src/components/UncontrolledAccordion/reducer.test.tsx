87de0893e9804321859592c75a3bb0b11313f581
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-09-24 22:33:13 +0300

    15 LESSON - IMPLEMENT reducer for test

--- src/components/UncontrolledAccordion/reducer.test.tsx
+++ src/components/UncontrolledAccordion/reducer.test.tsx
@@ -1,38 +1,32 @@
 import {reducer, StateType, TOGGLE_COLLAPSED} from "./UncontrolledAccordion";
 
 test("collapsed should be true", ()=>{
+    const state:StateType = {
+        collapsed: false
+    }
 
-const state: StateType = {
-    collapsed: false
-}
+    const endState = reducer(state, {type: TOGGLE_COLLAPSED})
 
-const endState = reducer(state, {type: TOGGLE_COLLAPSED})
+expect(endState.collapsed).toBe(true);
 
-
-    expect(endState.collapsed).toBe(true);
 })
-
 test("collapsed should be false", ()=>{
+    const state:StateType = {
+        collapsed: true
+    }
 
-const state: StateType = {
-    collapsed: true
-}
+    const endState = reducer(state, {type: TOGGLE_COLLAPSED})
 
-const endState = reducer(state, {type: TOGGLE_COLLAPSED})
+expect(endState.collapsed).toBe(false);
 
-
-    expect(endState.collapsed).toBe(false);
 })
+test("should return result", ()=>{
+    const state:StateType = {
+        collapsed: false
+    }
 
-test("error should be handled in case if incorrect action type added", ()=>{
+    expect(()=>{
+        reducer(state, {type: "FakeType"})
 
-const state: StateType = {
-    collapsed: false
-}
-
-
-expect(()=>{
-    reducer(state, {type: "FakeActionType"})
-
-}).toThrowError();
+    }).toThrowError();
 })
--- src/components/UncontrolledOnOff/UncontrolledOnOff.tsx
+++ src/components/UncontrolledOnOff/UncontrolledOnOff.tsx
@@ -1,8 +1,9 @@
+/*
 import React, {useState} from "react";
 
 type OnOffPropsType = {
     onChange : (on:boolean)=> void
-    defaultOn? : boolean
+    defaultOn : boolean
 }
 
 function UncontrolledOnOff(props:OnOffPropsType) {
@@ -55,6 +56,147 @@ return(
         <div style={IndicatorStyle}></div>
     </div>
 )
+}
+
+export default UncontrolledOnOff;*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React, {useReducer, useState} from "react";
+
+type OnOffPropsType = {
+    onChange : (on:boolean)=> void
+}
+type ActionType = {
+    type: string
+}
+
+export type StateType = {
+    on:boolean
+}
+
+export const reducer = (state: StateType, action:ActionType )=>{
+switch (action.type) {
+    case "on - true":
+        return{
+            ...state,
+            on: true
+        }
+        case "on - false":
+        return{
+            ...state,
+            on: false
+        }
+    default:
+        throw new Error("Incorrect state type was selected")
+}
+}
+
+
+function UncontrolledOnOff(props:OnOffPropsType) {
+    console.log("OnOffRendering")
+
+    let[state, dispatch] = useReducer(reducer, {on:false});
+
+    console.log("On: " + state.on)
+
+    const OnStyle={
+        width:"30px",
+        height:"20px",
+        border:"1px solid black",
+        display:"inline-block",
+        padding:"2px",
+        marginRight:"5px",
+        backgroundColor: state.on ? "green" : "white"
+    }
+    const OffStyle={
+        width:"30px",
+        height:"20px",
+        border:"1px solid black",
+        display:"inline-block",
+        padding:"2px",
+        marginRight:"5px",
+        backgroundColor: state.on ? "white" : "red"
+
+
+    }
+    const IndicatorStyle={
+        width:"10px",
+        height:"10px",
+        borderRadius:"5px",
+        border:"1px solid black",
+        display:"inline-block",
+        backgroundColor: state.on ? "green" : "red"
+    }
+
+    const onClicked=()=>{
+        props.onChange(true)
+        //setOn(true)
+        dispatch({type: "on - true"})
+
+    }
+    const offClicked =()=>{
+        props.onChange(false)
+        //setOn(false)
+        dispatch({type: "on - false"})
+
+    }
+
+    return(
+        <div>
+            <div style={OnStyle} onClick={onClicked}>On</div>
+            <div style={OffStyle} onClick={offClicked}>Off</div>
+            <div style={IndicatorStyle}></div>
+        </div>
+    )
 }
 
 export default UncontrolledOnOff;
