404d3600eddd19f329c8d96e1555059d0ed11336
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-09 14:09:42 +0300

    22 lesson - add Clock stories

---
+++ src/components/Clock_2_0/Clock_2_0.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Clock_2_0} from "./Clock_2_0";
+
+export default {
+    title: "Clock 2.0",
+    component: Clock_2_0
+}
+
+export const Clock_2_0_ = () =>{
+
+    return <Clock_2_0 />
+}
